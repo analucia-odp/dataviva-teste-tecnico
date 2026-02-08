9a5912f2af22bb1faf1594e446b1bbb10634d827
analucia-odp <analucia.odp@gmail.com>
2026-02-08 10:35:20 -0300

    feat: challenge two

--- desafios/desafio2.py
+++ desafios/desafio2.py
@@ -1,1 +1,14 @@
-""" Escreva aqui sua solução para o desafio 2 """
+def is_palindrome(word):
+    """ Check if the string 's' is a palindrome., 
+    ignoring case differences. """
+    cleaned_s = word.lower()  # Normalize for minuscule
+    return cleaned_s == cleaned_s[::-1] 
+
+def main():
+    while True:
+        word = input("Enter a word to check if it's a palindrome (or 'quit' to exit): ")
+        if word.lower() == "quit":
+            break
+        print(is_palindrome(word))
+
+main()
