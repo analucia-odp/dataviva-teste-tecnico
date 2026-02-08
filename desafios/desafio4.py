1c53195e5073d9762086b8884880c3ad72b1e106
analucia-odp <analucia.odp@gmail.com>
2026-02-08 12:09:09 -0300

    refact: new edge test case

--- desafios/desafio4.py
+++ desafios/desafio4.py
@@ -10,43 +10,38 @@ PARENTHESES_EXPECTED_OPENING = {
     ']': '['
 }
 
-def validate_closing_parentheses(s, char):
-    expected_closing = PARENTHESES_EXPECTED_CLOSING.get(char)
-    if expected_closing and expected_closing not in s:
+def validate_closing_parentheses(stack, char):
+    # Não pode fechar se não houve abertura
+    if not stack:
         return False
-    return True
 
-def validate_opening_parentheses(s, char):
-    expected_open = PARENTHESES_EXPECTED_CLOSING.get(char)
-    if expected_open and expected_open not in s:
-        return False
-    return True
+    last_open = stack.pop()
+    return PARENTHESES_EXPECTED_CLOSING[last_open] == char
     
 def validate_parentheses(s):
     """This function validates if the parentheses in the input string are correctly matched and nested."""
-    tam = len(s) - 1
+    stack = []
     
     for char in s:
         
         # First - If there is an opening parenthesis, there must be a corresponding closing parenthesis.
-        if not validate_closing_parentheses(s, char):
-            return "Invalid"
+         if char in PARENTHESES_EXPECTED_CLOSING:
+            stack.append(char)
         
         # Second - If there is a closing parenthesis, there must be a corresponding opening parenthesis.
-        if not validate_opening_parentheses(s, char):
-            return "Invalid"
+         elif char in PARENTHESES_EXPECTED_OPENING:
+            if not validate_closing_parentheses(stack, char):
+                return "Invalid"
         
         #Last - If there is an opening parenthesis, and there is a closing parenthesis, 
         # it must appear after the opening parenthesis in the correct position.
-        index = s.index(char)
-        validate = s[tam - index]
-        expected_closing = PARENTHESES_EXPECTED_CLOSING.get(char)
-        if expected_closing and validate != expected_closing:
-            return "Invalid"
     
-    return "Valid"
+    return "Valid" if not stack else "Invalid"
 
 # Test cases
+def test_validate_parenthses_simple():
+    assert validate_parentheses('[]()') == "Valid"
+    
 def test_validate_parentheses():
     assert validate_parentheses('{[()]}') == "Valid"
     
@@ -58,10 +53,15 @@ def test_missing_closing_parentheses():
 
 def main_tests():
     print("Running tests...")
-    print(f"Test 1: Valid parentheses '{[()]}'")
+    print(f"Test 1: Valid parentheses simple '[]()'")
+    test_validate_parenthses_simple()  # Expected output: Valid
+    
+    print(f"Test 2: Valid parentheses '{[()]}'")
     test_validate_parentheses()  # Expected output: Valid
+    
     print(f"Test 2: Invalid order parentheses '{{[(])}}'")
     test_invalid_order_parentheses()  # Expected output: Invalid
+    
     print(f"Test 3: Missing closing parentheses '{{[[(]]}}'")
     test_missing_closing_parentheses()  # Expected output: Invalid
     
